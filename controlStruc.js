31278c7bbfd6480415e49f74514a66716ff08ca2
Kanha Tomar <kanharajput91@gmail.com>
2023-08-16 18:39:58 +0530

    if and for

---
+++ controlStruc.js
@@ -0,0 +1,15 @@
+const name = "kuldeep";
+
+if(name === "kanha"){
+    console.log("Hi baby");
+}else if(name === "kuldeep"){
+    console.log("Hi Cool")
+}else{
+    console.log("Hi sabko");
+}
+
+const students = ["kanha", "gopal", "mohit"];
+
+for(const student of students){
+    console.log(student);
+}
