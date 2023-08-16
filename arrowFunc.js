262e2c292c341e4b1fec724e529fbfc4d54191e6
Kanha Tomar <kanharajput91@gmail.com>
2023-08-16 16:55:03 +0530

    objects

--- arrowFunc.js
+++ arrowFunc.js
@@ -8,3 +8,13 @@ export default (name, message) => {
     return "nothing";
 }
 */
+/*
+More on arrow function
+if there only a parameter then we can remove the paranthesis, 
+if there's no logic only a return statement then we can remove the return as well as curly brackets.
+
+// we want to create an object and return it in anonymous function so wrap it inside the curly bracket and then it will be noted as object otherwise js understand it as the anonumous function defination
+/* Example
+() => ({ age: number })
+
+*/ 
---
+++ objects.js
@@ -0,0 +1,10 @@
+// define a object name with lowercase letters
+const user = {
+    name: "kanha",
+    second_name: "tomar",
+}
+
+console.log(user);
+console.log(user.name);
+console.log(user.second_name);
+
